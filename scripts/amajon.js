0f16008d99e73bacdb63b9e4bbb0a8145cc62058
ankitaSingh <iankitasing15@gmail.com>
2023-06-14 20:19:23 +0530

    make the cart quantity interactive

--- scripts/amajon.js
+++ scripts/amajon.js
@@ -83,7 +83,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
                 quantity : 1
             });
         }
-       
-        console.log(cart)
+
+        let cartquantity  = 0;
+
+        cart.forEach((item)=>{
+             cartquantity+=item.quantity;
+        })
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartquantity
+
+        
       });
 });
